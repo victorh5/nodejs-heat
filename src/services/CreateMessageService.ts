4b13a6e99f1fbf07a1af84a9136362ec646e4cfb
Victor Hugo <61998936+victorh5@users.noreply.github.com>
2021-10-21 23:58:28 -0400

    criação das mensagens, tanto controller e service

--- src/services/CreateMessageService.ts
+++ src/services/CreateMessageService.ts
@@ -1,3 +1,4 @@
+import { io } from "../app";
 import prismaClient from "../prisma";
 
 class CreateMessageService {
@@ -12,6 +13,18 @@ class CreateMessageService {
         user: true
       }
     });
+
+    const infoWs = {
+      text: message.text,
+      user_id: message.user_id,
+      created_at: message.create_at,
+      user: {
+        name: message.user.name,
+        avatar_url: message.user.avatar_url,
+      }
+    }
+
+    io.emit("new_message", infoWs)
 
     return message;
   }
